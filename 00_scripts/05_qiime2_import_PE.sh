db2fb0b610c5aefbb49f03d3b7eac84a7a20a06a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-03-17 17:00:59 +0100

    Update 05_qiime2_import_PE.sh

--- 00_scripts/05_qiime2_import_PE.sh
+++ 00_scripts/05_qiime2_import_PE.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/03_cleaned_data/Original_reads_16S
-OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/05_QIIME2/Original_reads_16S
+WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/03_cleaned_data/Original_reads_16S
+OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/05_QIIME2/Original_reads_16S
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -10,7 +10,7 @@ mkdir -p $OUTPUT
 # In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
 # See https://docs.qiime2.org/2018.8/tutorials/importing/
 
-MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/98_database_files/manifest_16S
+MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/98_database_files/manifest_16S
 TMPDIR=/scratch_vol0
 
 ###############################################################
@@ -45,8 +45,8 @@ cd $OUTPUT
 # qiime tools export --input-path visual/demux.qzv --output-path export/visual/demux
 # qiime tools export --input-path core/demux.qza --output-path export/core/demux
 
-WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/03_cleaned_data/Original_reads_18S
-OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/05_QIIME2/Original_reads_18S
+WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/03_cleaned_data/Original_reads_18S
+OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/05_QIIME2/Original_reads_18S
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -55,53 +55,7 @@ mkdir -p $OUTPUT
 # In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
 # See https://docs.qiime2.org/2018.8/tutorials/importing/
 
-MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/98_database_files/manifest_18S
-TMPDIR=/scratch_vol0
-
-###############################################################
-### For importing your data in a Qiime2 format
-###############################################################
-
-cd $WORKING_DIRECTORY
-
-eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p $OUTPUT/core
-mkdir -p $OUTPUT/visual
-
-# I'm doing this step in order to deal the no space left in cluster :
-export TMPDIR='/scratch_vol0/fungi'
-echo $TMPDIR
-
-qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
-			    --input-path  $MANIFEST \
-			    --output-path $OUTPUT/core/demux.qza \
-			    --input-format PairedEndFastqManifestPhred33V2
-
-cd $OUTPUT
-
-# qiime demux summarize --i-data core/demux.qza --o-visualization visual/demux.qzv
-
-# for vizualisation :
-# https://view.qiime2.org
-
-# qiime tools export --input-path visual/demux.qzv --output-path export/visual/demux
-# qiime tools export --input-path core/demux.qza --output-path export/core/demux
-
-
-WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/03_cleaned_data/Original_reads_ITS
-OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/05_QIIME2/Original_reads_ITS
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p $OUTPUT
-
-# You need a "manifest" for explaining and importing your data :
-# In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
-# See https://docs.qiime2.org/2018.8/tutorials/importing/
-
-MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/98_database_files/manifest_ITS
+MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/98_database_files/manifest_18S
 TMPDIR=/scratch_vol0
 
 ###############################################################
@@ -137,8 +91,8 @@ cd $OUTPUT
 # qiime tools export --input-path core/demux.qza --output-path export/core/demux
 
 
-WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/03_cleaned_data/Original_reads_TUFA
-OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/05_QIIME2/Original_reads_TUFA
+WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/03_cleaned_data/Original_reads_ITS
+OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/05_QIIME2/Original_reads_ITS
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -147,7 +101,53 @@ mkdir -p $OUTPUT
 # In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
 # See https://docs.qiime2.org/2018.8/tutorials/importing/
 
-MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_Lagoon_II/98_database_files/manifest_TUFA
+MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/98_database_files/manifest_ITS
+TMPDIR=/scratch_vol0
+
+###############################################################
+### For importing your data in a Qiime2 format
+###############################################################
+
+cd $WORKING_DIRECTORY
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2021.4
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT/core
+mkdir -p $OUTPUT/visual
+
+# I'm doing this step in order to deal the no space left in cluster :
+export TMPDIR='/scratch_vol0/fungi'
+echo $TMPDIR
+
+qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
+			    --input-path  $MANIFEST \
+			    --output-path $OUTPUT/core/demux.qza \
+			    --input-format PairedEndFastqManifestPhred33V2
+
+cd $OUTPUT
+
+# qiime demux summarize --i-data core/demux.qza --o-visualization visual/demux.qzv
+
+# for vizualisation :
+# https://view.qiime2.org
+
+# qiime tools export --input-path visual/demux.qzv --output-path export/visual/demux
+# qiime tools export --input-path core/demux.qza --output-path export/core/demux
+
+
+WORKING_DIRECTORY=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/03_cleaned_data/Original_reads_TUFA
+OUTPUT=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/05_QIIME2/Original_reads_TUFA
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+# You need a "manifest" for explaining and importing your data :
+# In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
+# See https://docs.qiime2.org/2018.8/tutorials/importing/
+
+MANIFEST=/scratch_vol0/fungi/BioIndic_La_Reunion_Island_seawater_four_month/98_database_files/manifest_TUFA
 TMPDIR=/scratch_vol0
 
 ###############################################################
