d6de2292bda4190db929c3eaf258b26dd10b6d96
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-03-18 08:59:09 +0100

    Update 08_qiime2_rarefaction_PE.sh

--- 00_scripts/08_qiime2_rarefaction_PE.sh
+++ 00_scripts/08_qiime2_rarefaction_PE.sh
@@ -28,7 +28,7 @@ echo $TMPDIR
 qiime diversity alpha-rarefaction \
   --i-table core/Table.qza \
   --i-phylogeny tree/rooted-tree.qza \
-  --p-max-depth 25112 \
+  --p-max-depth 27500 \
   --p-min-depth 1 \
   --m-metadata-file $DATABASE/sample-metadata.tsv \
   --o-visualization visual/alpha-rarefaction.qzv
@@ -91,7 +91,7 @@ echo $TMPDIR
 qiime diversity alpha-rarefaction \
   --i-table core/Table.qza \
   --i-phylogeny tree/rooted-tree.qza \
-  --p-max-depth 33102 \
+  --p-max-depth 26403 \
   --p-min-depth 1 \
   --m-metadata-file $DATABASE/sample-metadata.tsv \
   --o-visualization visual/alpha-rarefaction.qzv
@@ -153,7 +153,7 @@ echo $TMPDIR
 qiime diversity alpha-rarefaction \
   --i-table core/Table.qza \
   --i-phylogeny tree/rooted-tree.qza \
-  --p-max-depth 16687 \
+  --p-max-depth 12839 \
   --p-min-depth 1 \
   --m-metadata-file $DATABASE/sample-metadata.tsv \
   --o-visualization visual/alpha-rarefaction.qzv
@@ -217,7 +217,7 @@ echo $TMPDIR
 qiime diversity alpha-rarefaction \
   --i-table core/Table.qza \
   --i-phylogeny tree/rooted-tree.qza \
-  --p-max-depth 124155 \
+  --p-max-depth 81360 \
   --p-min-depth 1 \
   --m-metadata-file $DATABASE/sample-metadata.tsv \
   --o-visualization visual/alpha-rarefaction.qzv
